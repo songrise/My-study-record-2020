86bb499fffc64e460882fa07eacaec40f52fd8ad
JIANG-Rx <54588043+JIANG-Rx@users.noreply.github.com>
2020-03-14 15:59:11 +0800

    test command line commit

--- C/CSAPP-Lab/source/datalab-handout/bits.c
+++ C/CSAPP-Lab/source/datalab-handout/bits.c
@@ -142,7 +142,7 @@ int bitAnd(int x, int y)
   return ~((~x) | (~y));
 }
 /* 
- * getByte - Extract byte n from word x
+ * getByte - Extract byte n from word x.,
  *   Bytes numbered from 0 (LSB) to 3 (MSB)
  *   Examples: getByte(0x12345678,1) = 0x56
  *   Legal ops: ! ~ & ^ | + << >>
