04b9ff36352cb7cc95fdf9f41b5e7ce711a24268
JIANG-Rx <54588043+JIANG-Rx@users.noreply.github.com>
2020-03-16 23:36:03 +0800

    few cpp practice

---
+++ C++/20_03/initIntToPtr.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int *address = 0x00600000;
+    // int *address = (int *)0x00600000;
+    //cast is needed for c++
+    return 0;
+}
--- C/Leetcode/2020-3/88_mergeSortedArray.c
+++
@@ -1,4 +0,0 @@
-
-void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n)
-{
-}
