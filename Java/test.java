c25dcc9f5d4a89b52b22a4e51a2cc28197d4bdf8
JIANG-Rx <54588043+JIANG-Rx@users.noreply.github.com>
2020-05-19 00:50:46 +0800

    first java file

--- Java/test.java
+++ Java/test.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+public class test {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Java!");
     }
 }
