4f5c868fb532ea86502ac85ddfb57a53d3b8e190
S0ngR1sE <54588043+songrise@users.noreply.github.com>
2020-08-19 11:06:25 +0800

    updated gitignore

--- Python/20_08/n_determinant.py
+++ Python/20_08/n_determinant.py
@@ -63,3 +63,7 @@ class determinant():
 
 zero1 = determinant([[1, -2, -3], [2, -4, -6], [-4, 1, -2]])
 zero1.ShowInfo()
+
+test1 = determinant([[1, 3, 1, 2], [1, 5, 3, -4],
+                     [0, 4, 1, -1], [-5, 1, 3, -6]])
+test1.ShowInfo()
